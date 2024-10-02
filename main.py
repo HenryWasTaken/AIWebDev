9652ec7f86626a673d447434e92d6a1752add885
HenryWasTaken <85043968+HenryWasTaken@users.noreply.github.com>
2024-10-02 16:35:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Streamlit App Title with different text styles
 st.markdown("<h1 style='color:white;'>StudyGPT</h1>", unsafe_allow_html=True)
-st.markdown("<p style='color:grey; font-size: small;'>This is a demo of StudyGPT, a set of prompts designed to help students. To use, simply type the thing you need help with. The model will then guide you to solving your problems! This is still a prototype. Still check important info</p>", unsafe_allow_html=True)
+st.markdown("<p style='color:grey; font-size: small;'>StudyGPT uses a set of prompts designed to help students. To use, simply type the thing you need help with. The model will then guide you to solving your problems! This is still a prototype. Still check important info</p>", unsafe_allow_html=True)
 st.markdown("<p style='color:white;'>This model is not intended to give a 'quick' answer to your last-minute homework, and is not a substitute for a teacher.</p>", unsafe_allow_html=True)
 
 # Add the contact information
