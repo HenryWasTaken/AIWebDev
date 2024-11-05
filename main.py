f50bdb16cc3025fc448580dfe640aebca0561fb1
HenryWasTaken <85043968+HenryWasTaken@users.noreply.github.com>
2024-11-05 16:02:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ st.markdown("<p style='color:white;'>Important: This GPT does not log or store a
 with st.expander("The GPT's Mission!"):
     st.markdown(f"<p style='color:white;'>{system_prompt}</p>", unsafe_allow_html=True)
 
-# Adding Sidebar
-st.sidebar.markdown("## Sidebar")
-
+st.page_link("main.py", label="Home", icon="Home")
+st.page_link("AIVision.py", label="AI Vision")
+=
 # New Chat button at the top of the sidebar
 if st.button("Reset Chat"):
     # Reset the conversation history
