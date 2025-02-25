eef190ce164d7cbadd39064bb472ad84a7398a2e
HenryWasTaken <85043968+HenryWasTaken@users.noreply.github.com>
2025-02-25 16:00:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -55,14 +55,6 @@ def load_notes():
         return []
 
 # Function to extract text from a PDF
-'''
-def extract_text_from_pdf(uploaded_file):
-    reader = PdfReader(uploaded_file)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
-'''
 
 def extract_text_from_pdf(uploaded_file):
     reader = PdfReader(uploaded_file)
